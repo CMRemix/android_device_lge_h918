78ea2fa92465c0a2da270dc379e7589302ce5036
ZION959 <zionmtran43@gmail.com>
2017-01-16 12:47:09 -0700

    switch to cmremix_h918_defconfig
    Change-Id: I271c760a7c98415969a87c156e2e869a5d7da3a3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ DEVICE_PATH := device/lge/h918
 TARGET_OTA_ASSERT_DEVICE := h918
 
 # Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_h918_defconfig
+TARGET_KERNEL_CONFIG := cmremix_h918_defconfig
 
 # inherit from the proprietary version
 -include vendor/lge/h918/BoardConfigVendor.mk
